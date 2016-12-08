8f279d240e4bcc78052b21ccd7a661bc31e6eae2
John Curatcha <curatchaj@gmail.com>
2016-12-07 20:00:10 -0600

    Framework for learning how to use volley

--- app/src/main/java/jcuratcha/weather/Clients/WeatherInformationClient.java
+++ app/src/main/java/jcuratcha/weather/Clients/WeatherInformationClient.java
@@ -1,8 +1,13 @@
 package jcuratcha.weather.Clients;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 /**
  * Created by DoorCrasher on 2016-12-07.
  */
 
 public class WeatherInformationClient {
+
+
 }
--- app/src/main/java/jcuratcha/weather/Dashboard.java
+++ app/src/main/java/jcuratcha/weather/Dashboard.java
@@ -2,12 +2,44 @@ package jcuratcha.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class Dashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final TextView mTextView = (TextView) findViewById(R.id.request_text);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://www.google.ca";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                mTextView.setText("Response is: " + response.substring(0, 500));
+            }
+        },
+        new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+
+        });
+
+
+
         setContentView(R.layout.activity_dashboard);
     }
+
+
 }
