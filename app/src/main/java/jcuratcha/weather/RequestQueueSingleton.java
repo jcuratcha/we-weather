515f479f402bbe21f02c90048fa6f0e333e1dd1a
DoorCrasher <jcuratcha_@live.com>
2017-01-01 21:52:26 -0600

    Changed request queue singleton to make it follow standards - no longer subclassing Application - adding ImageLoader functionality to the singleton

--- app/src/main/java/jcuratcha/weather/RequestQueueSingleton.java
+++ app/src/main/java/jcuratcha/weather/RequestQueueSingleton.java
@@ -1,39 +1,70 @@
 package jcuratcha.weather;
 
 import android.app.Application;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
-public class RequestQueueSingleton extends Application {
+public class RequestQueueSingleton {
 
     private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private Context mContext;
+
     private static RequestQueueSingleton mInstance;
 
     public static final String TAG = RequestQueueSingleton.class.getName();
 
-    @Override
-    public void onCreate(){
-        super.onCreate();
-        mInstance = this;
-        mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+    private RequestQueueSingleton(Context context) {
+        mContext = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> cache = new LruCache<>(20);
+
+            @Override
+            public Bitmap getBitmap(String url) {
+                return cache.get(url);
+            }
+
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                cache.put(url, bitmap);
+            }
+        });
     }
 
-    public RequestQueue getmRequestQueue() {
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+
         return mRequestQueue;
     }
 
     public <T> void add(Request<T> req) {
         req.setTag(TAG);
-        getmRequestQueue().add(req);
+        getRequestQueue().add(req);
     }
 
     public void cancel() {
         mRequestQueue.cancelAll(TAG);
     }
 
-    public static synchronized RequestQueueSingleton getInstance() {
+    public static synchronized RequestQueueSingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new RequestQueueSingleton(context);
+        }
+
         return mInstance;
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
     }
 }
