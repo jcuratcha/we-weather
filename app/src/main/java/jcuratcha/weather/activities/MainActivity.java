d05788b6afde4354d885dbc82b3b28ad4885128a
DoorCrasher <jcuratcha_@live.com>
2017-01-11 17:18:14 -0600

    Added settings activity, modify default settings to include weather specific user settings - still need to make the settings save in SharedPreferences

--- app/src/main/java/jcuratcha/weather/activities/MainActivity.java
+++ app/src/main/java/jcuratcha/weather/activities/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -59,6 +61,24 @@ public class MainActivity extends AppCompatActivity {
 
             prefs.edit().putBoolean("firstRun", false).apply();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this, UserSettingsActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void GetCurrentWeatherData(View view) {
